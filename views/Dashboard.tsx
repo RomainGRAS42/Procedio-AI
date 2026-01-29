e735fd0c03a5be99489d62245184ac15c9dba11c
Romain Gras <ssnbach2005@gmail.com>
2026-01-29 09:42:57 +0100

    fix(dashboard): add missing createPortal import

--- views/Dashboard.tsx
+++ views/Dashboard.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { createPortal } from "react-dom";
 import { User, Procedure, UserRole, Suggestion } from "../types";
 import { supabase } from "../lib/supabase";
 
